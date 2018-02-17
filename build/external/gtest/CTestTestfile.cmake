fb72097b24e1bc2617ac8b3ac956c58fe99a310b
hornie <hornminer@gmail.com>
2018-02-17 09:20:59 +0300

    Remove build dir

--- build/external/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/HLEBcoin/external/gtest
-# Build directory: /home/HLEBcoin/build/external/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/external/miniupnpc/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/HLEBcoin/external/miniupnpc
-# Build directory: /home/HLEBcoin/build/external/miniupnpc
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
